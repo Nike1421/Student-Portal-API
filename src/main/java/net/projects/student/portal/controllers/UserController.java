968b98e8f2f0f745ac646140273f7ea3cd4f1412
Nike1421 <ommiemo54@gmail.com>
2023-06-18 00:13:21 +0530

    Added Preliminary UserController

---
+++ src/main/java/net/projects/student/portal/controllers/UserController.java
@@ -0,0 +1,49 @@
+package net.projects.student.portal.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import net.projects.student.portal.models.User;
+import net.projects.student.portal.repository.UserRepository;
+import net.projects.student.portal.security_config.jwt_services.JwtUtils;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/user")
+public class UserController {
+// CHECK THIS PLEASE MUST HAVE ERRORS
+	@Autowired
+	UserRepository userRepository;
+
+//	@Autowired
+//	private UserDetailsServiceImpl userDetailsService;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	public ResponseEntity<?> getSelfDetails(HttpServletRequest request) {
+		String jwtString = jwtUtils.getJwtFromCookies(request);
+
+		try {
+
+			User user = null;
+			if (jwtString != null && jwtUtils.validateJwtToken(jwtString)) {
+				String usernameString = jwtUtils.getUserNameFromJwtToken(jwtString);
+
+				user = userRepository.findBySapId(usernameString).orElseThrow(
+						() -> new UsernameNotFoundException("User Not Found with SAP ID " + usernameString));
+
+			}
+			return ResponseEntity.ok().body(user);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return ResponseEntity.badRequest().build();
+		}
+
+	}
+}
