ac123192eca01dc81f117c4eb07bc928b9a040ea
Nike1421 <ommiemo54@gmail.com>
2023-06-17 16:14:08 +0530

    Changed Validation Constraint Import

--- src/main/java/net/projects/student/portal/models/User.java
+++ src/main/java/net/projects/student/portal/models/User.java
@@ -3,13 +3,12 @@ package net.projects.student.portal.models;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
